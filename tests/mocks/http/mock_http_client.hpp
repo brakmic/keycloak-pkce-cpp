1d8e7699d83d67687a8b68a5a9f5047125bba2a7
Harris Brakmic <brakmic@gmail.com>
2025-03-02 14:54:46 +0000

    fix: http client mock

--- tests/mocks/http/mock_http_client.hpp
+++ tests/mocks/http/mock_http_client.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "keycloak/http/http_client.hpp"
+#include "keycloak/http/ssl_config.hpp"
+#include "keycloak/http/proxy_config.hpp"
 #include <unordered_map>
 #include <string>
 #include <utility>
@@ -9,24 +11,24 @@ namespace keycloak::test {
 
 class MockHttpClient {
 public:
-    static http::HttpClient::Response post(
+    static http::Response post(
         const std::string& host [[maybe_unused]],
         const std::string& port [[maybe_unused]],
         const std::string& path [[maybe_unused]],
         const std::string& body [[maybe_unused]],
         const std::unordered_map<std::string, std::string>& headers [[maybe_unused]],
-        const http::HttpClient::SSLConfig& ssl_config [[maybe_unused]],
-        const http::HttpClient::ProxyConfig& proxy_config [[maybe_unused]]
+        const http::SSLConfig& ssl_config [[maybe_unused]],
+        const http::ProxyConfig& proxy_config [[maybe_unused]]
     ) {
         if (host == "httpbin.org") {
-            http::HttpClient::Response response;
+            http::Response response;
             response.status_code = 200;
             response.body = "{\"success\":true}";
             response.headers = {{"Content-Type", "application/json"}};
             return response;
         }
 
-        http::HttpClient::Response response;
+        http::Response response;
         response.status_code = 500;
         response.body = "Host not found";
         response.headers = {};
